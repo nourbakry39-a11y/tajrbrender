2794848b0389894a66c302ae6b94606438f6f8f3
nour bakry <nourbakry39@gmail.com>
2026-04-02 17:14:58 +0200

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("Hello from Render!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
